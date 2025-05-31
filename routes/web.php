92324d0620da2b1695449b0d2cf4c6c6032ce52b
Xoliswa Masuku <bester12@outlook.com>
2025-05-31 02:48:20 +0200

    Cleaned repo: updated .gitignore and removed tracked sensitive/ignored files

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\NatureController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register web routes for your Application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
